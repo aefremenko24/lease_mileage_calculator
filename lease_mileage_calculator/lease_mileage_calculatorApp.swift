4f8ed649d38d0562e82135421a5e71977b23cb36
Arthur Efremenko <aefremenko24@gmail.com>
2025-05-09 18:49:25 -0400

    Initial Commit

---
+++ lease_mileage_calculator/lease_mileage_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  lease_mileage_calculatorApp.swift
+//  lease_mileage_calculator
+//
+//  Created by Arthur Efremenko on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct lease_mileage_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lease_mileage_calculatorTests/lease_mileage_calculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  lease_mileage_calculatorTests.swift
+//  lease_mileage_calculatorTests
+//
+//  Created by Arthur Efremenko on 5/9/25.
+//
+
+import Testing
+@testable import lease_mileage_calculator
+
+struct lease_mileage_calculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
