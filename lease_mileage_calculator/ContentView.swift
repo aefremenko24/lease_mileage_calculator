fb9992d45488df67b89b5d8d5e694f6843515544
Arthur Efremenko <aefremenko24@gmail.com>
2025-05-12 12:14:34 -0400

    Persistance fix

--- lease_mileage_calculator/ContentView.swift
+++ lease_mileage_calculator/ContentView.swift
@@ -11,8 +11,10 @@ struct ContentView: View {
     @State var purchaseDate: Date = Date()
     @State var milesPerYear: String = ""
     
-    @AppStorage("purchase_date") var purchaseDateStorage: Date?
-    @AppStorage("miles_per_year") var milesPerYearStorage: String?
+    @AppStorage("purchase_date") var purchaseDateStorage: Date = Date()
+    @AppStorage("miles_per_year") var milesPerYearStorage: String = ""
+    
+    @FocusState private var showKeyboard: Bool
     
     var body: some View {
         VStack(alignment: .center, spacing: 20) {
@@ -20,31 +22,46 @@ struct ContentView: View {
                        selection: $purchaseDate,
                        in: ...Date(),
                        displayedComponents: [.date])
-                .onSubmit {
-                    purchaseDateStorage = purchaseDate
-                }
+            .onAppear() {
+                purchaseDate = purchaseDateStorage
+            }
+            .onChange(of: purchaseDate) {
+                purchaseDateStorage = purchaseDate
+            }
             
             HStack {
                 Text("# of miles per year")
                 Spacer()
                 TextField("", text: $milesPerYear)
                     .keyboardType(.numberPad)
-                    .onSubmit {
+                    .onAppear() {
+                        milesPerYear = milesPerYearStorage
+                    }
+                    .onChange(of: milesPerYear) {
                         milesPerYear = milesPerYear.filter { "0123456789".contains($0) }
                         milesPerYearStorage = milesPerYear
                     }
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .frame(width: 120)
+                    .focused($showKeyboard)
             }
             
             Spacer()
             
-            Text("You should be at or under")
-            Text("\(getSuggestedMiles())")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            Text("miles")
-                .padding(.top, -10)
+            VStack {
+                Text("You should be at or under")
+                Text("\(getSuggestedMiles())")
+                    .scaledToFit()
+                    .padding(.horizontal, 40)
+                    .font(.system(size: 100, weight: .bold))
+                    .minimumScaleFactor(0.01)
+                Text("miles")
+            }
+            .frame(width: UIScreen.main.bounds.size.width - 40)
+            .onTapGesture {
+                showKeyboard = false
+            }
+            .sensoryFeedback(.selection, trigger: showKeyboard)
             
             Spacer()
                 
